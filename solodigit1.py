207f0b2f596f4f1f06cb766f3998991e9946be99
balamurugan1999v <31644214+balamurugan1999v@users.noreply.github.com>
2019-06-06 22:22:42 +0530

    Update and rename solodigit.py to solodigit1.py

--- solodigit1.py
+++ solodigit1.py
@@ -3,6 +3,8 @@ n=int(input())
 sets=input().split()
 for i in sets:
   s1.append(i)
+b=0
+u=0
 for i in range(0,n-1,1):
   b=0
   for j in range(0,n,1):
@@ -10,5 +12,6 @@ for i in range(0,n-1,1):
       if s1[i]==s1[j]:
         b=b+1
   if b==0:
-      print (s1[i])
-      break
+    u=i
+    break
+print(s1[u])
